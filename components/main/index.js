83d5880ec740caf945a9760d3250672238c5ee57
ManhThang03 <148290463+ManhThang203@users.noreply.github.com>
2025-08-08 08:28:00 +0700

    add home page data

--- components/main/index.js
+++ components/main/index.js
@@ -1,3 +1,4 @@
+import httpRequest from "../../utils/httpRequest.js";
 class MyHome extends HTMLElement {
   constructor() {
     super();
@@ -6,21 +7,72 @@ class MyHome extends HTMLElement {
 
   connectedCallback() {
     this.render();
-    this.getElements();
   }
   getElements() {
-    this.contentWrapper = this.shadowRoot.querySelector(".content-wrapper");
-    console.log(this.contentWrapper);
+    this.hitsGrid = this.shadowRoot.querySelector("#hits-grid");
+    this.artistsGrid = this.shadowRoot.querySelector("#artists-grid");
   }
   async render() {
     const data = await this.getHTMLString();
     this.shadowRoot.innerHTML = data;
+    this.getElements();
+    this.renderPlaylists();
+    this.renderArtists();
   }
 
   async getHTMLString() {
     const res = await fetch("./components/main/main.html");
     const data = await res.text();
     return data;
+  }
+  async renderPlaylists() {
+    const { playlists, pagination } = await httpRequest.get(
+      "playlists?limit=20&offset=0"
+    );
+    const html = playlists
+      .map(
+        (item) => `
+        <div class="hit-card">
+      <div class="hit-card-cover">
+        <img src="${item.image_url}" alt="Flowers" />
+        <button class="hit-play-btn">
+          <i class="fas fa-play"></i>
+        </button>
+      </div>
+      <div class="hit-card-info">
+        <h3 class="hit-card-title">${item.name}</h3>
+        <p class="hit-card-artist">${item.user_display_name}</p>
+      </div>
+    </div>
+    `
+      )
+      .join("");
+    this.hitsGrid.innerHTML = html;
+  }
+  async renderArtists() {
+    const { artists, pagination } = await httpRequest.get(
+      "artists?limit=20&offset=0"
+    );
+    const html = artists
+      .map(
+        (item) => `
+         <div class="artist-card">
+      <div class="artist-card-cover">
+        <img src="${item.image_url}" alt="${item.name}" />
+        <button class="artist-play-btn">
+          <i class="fas fa-play"></i>
+        </button>
+      </div>
+      <div class="artist-card-info">
+        <h3 class="artist-card-name">${item.name}</h3>
+        <p class="artist-card-type">${item.bio}</p>
+      </div>
+    </div>
+    `
+      )
+      .join("");
+
+    this.artistsGrid.innerHTML = html;
   }
   open() {
     document.querySelector(".content-wrapper").prepend(this);
--- components/sidebar/sidebar.js
+++ components/sidebar/sidebar.js
@@ -1,3 +1,5 @@
+import httpRequest from "../../utils/httpRequest.js";
+
 class sidebar extends HTMLElement {
   constructor() {
     super();
@@ -6,14 +8,96 @@ class sidebar extends HTMLElement {
   connectedCallback() {
     this.render();
   }
+  setListeners() {
+    this.sortBtn = this.shadowRoot.querySelector("#sort-btn");
+    this.sortForm = this.shadowRoot.querySelector("#sort-form");
+    this.sortContentDiv = this.shadowRoot.querySelectorAll("#sort-content div");
+    this.sortContentIcon = this.shadowRoot.querySelectorAll(
+      "#sort-content div i"
+    );
+    this.buttonForm = this.shadowRoot.querySelectorAll("#content button");
+    this.buttonFormIcon = this.shadowRoot.querySelectorAll("#content button i");
+    this.libraryContent = this.shadowRoot.querySelector("#library-content");
+    this.btnNear = this.shadowRoot.querySelector("#btn-near");
+    this.btnHamburger = this.shadowRoot.querySelector("#btn-hamburger");
+
+    this.sortBtn.addEventListener("click", (e) => {
+      e.stopPropagation();
+      this.toggleMenuSort();
+    });
+    document.addEventListener("click", (e) => {
+      const isSortForm = e.composedPath().includes(this.sortForm);
+      if (!isSortForm) {
+        this.sortForm.classList.remove("show");
+      }
+    });
+    document.addEventListener("keydown", (e) => {
+      if (e.key === "Escape") {
+        this.sortForm.classList.remove("show");
+      }
+    });
+    this.sortContentDiv.forEach((item) => {
+      item.addEventListener("click", () => {
+        this.sortContentDiv.forEach((div) => div.classList.remove("active"));
+        item.classList.add("active");
+        this.sortContentIcon.forEach((icon) => icon.classList.remove("active"));
+        // Thêm class active cho icon trong div được click
+        const icon = item.querySelector("i");
+        if (icon) {
+          icon.classList.add("active");
+        }
+        const value = item.textContent.trim().toLowerCase();
+      });
+    });
+    this.buttonForm.forEach((item, index) => {
+      item.addEventListener("click", () => {
+        this.buttonForm.forEach((item) => item.classList.remove("active"));
+        item.classList.add("active");
+      });
+    });
+  }
   async render() {
     const data = await this.getHTMLString();
     this.shadowRoot.innerHTML = data;
+    this.setListeners();
+    this.renderRecent();
   }
   async getHTMLString() {
     const res = await fetch("./components/sidebar/sidebar.html");
     const data = await res.text();
     return data;
+  }
+  toggleMenuSort() {
+    this.sortForm.classList.toggle("show");
+  }
+  // hàm render khi bấm vào nút gần đây
+  async renderRecent() {
+    const { artists } = await httpRequest.get("artists");
+    const likeSongsHtml = `
+     <div class="library-item">
+          <div class="item-info">
+            <div class="item-title">
+            <i class="fas fa-thumbtack"></i>
+              <p>bài hát đã thích</p>
+            </div>
+            <div class="item-subtitle">Danh sách bài hát</div>
+          </div>
+        </div>
+    `;
+    const htmlRecent = artists
+      .map(
+        (item) => `
+      <div class="library-item">
+          <div class="item-info">
+            <div class="item-title">${item.name}</div>
+            <div class="item-subtitle">Nghệ sĩ</div>
+          </div>
+        </div>
+    `
+      )
+      .join("");
+    this.libraryContent.innerHTML = likeSongsHtml + htmlRecent;
+    // this.artistsGrid.innerHTML = "";
   }
 }
 customElements.define("my-sidebar", sidebar);
